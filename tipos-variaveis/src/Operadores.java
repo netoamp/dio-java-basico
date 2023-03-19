698689a1e8ac5b4da7ed785f0d0179f9e3f35a5d
netoamp <netoamp@hotmail.com>
2023-03-18 23:12:37 -0300

    descrição dos operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,32 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+         /*OPERADORES*/
+         //Atribuição "="
+         String nome = "ALBERTO";
+         int idade = 32;
+         double peso = 98.5;
+         char sexo = 'M';
+         boolean doadorOrgao = false;
+         Date dataNascimento = new Date();
+
+         //Aritiméticos
+         double soma = 10.5 +15.7;
+         int subtracao = 113 - 25;
+         int multiplicacao = 20 * 7;
+         int divisao = 15 / 3;
+         int modulo = 18 % 3;
+         double resultado = (10 * 7) + (20 / 4);
+
+        // operador ternario
+        int a, b;
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ? "Verdadeiro" : "falso";
+
+        //A expressão acima abevia um uso de IF
+        
+        
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*CONSTANTES são variaveis que não devem sofrer alteração, por convenção são escritas em CAIXA ALTA
+         para garantir que isso ocorra, usa-se a palavra final na declaração*/
+
+         final double PI = 3.14;
+
+        
+        
     }
 }
